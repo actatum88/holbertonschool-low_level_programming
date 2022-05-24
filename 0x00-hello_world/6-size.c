7dad4088cda3736cc4a6c7fef8856d1765f1bce0
actatum88 <4946@holbertonschool.com>
2022-05-24 13:09:45 -0700

    learning C language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+   /**
+    * main - Entry point
+    *
+    * Return: Always 0 (Success)
+    */
+int main(void)
+{
+  printf ("Size of a char: %lu byte(s)\n", sizeof (char));
+  print f ("Size of an int: %lu byte(s)\n", sizeof (int));
+  print f("Size of a long int: %lu byte(s)\n", sizeof(double));
+  print f ("Size of a long long int: %lu byte(s)\n", size of (double));
+  print f ("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+
+}
