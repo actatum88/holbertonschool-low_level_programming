fff520b01423fc5098244441f1b6ae544f413a6d
actatum88 <4946@holbertonschool.com>
2022-05-24 19:53:17 -0700

    Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Task 2 to play alphabet game
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+  char x;
+
+  for (x = 'a' ; a <= 'z'; x++)
+    {
+      putchar(x);
+    }
+  putchar ('\n');
+
+  return (0);
+}
