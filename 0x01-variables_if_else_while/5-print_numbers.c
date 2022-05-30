c19a62ee113768fc52c75eff7623d83dbeba4ffe
actatum88 <4946@holbertonschool.com>
2022-05-30 11:54:19 -0700

    Variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+printf("\n");
+
+return (0);
+}
