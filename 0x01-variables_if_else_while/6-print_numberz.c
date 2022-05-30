cac2aaf4ce1b10e33d8157a68a8a94ec6941bdde
actatum88 <4946@holbertonschool.com>
2022-05-30 12:42:16 -0700

    variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - print numbers base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+	for (x = 10; x >= 0; x--)
+	{
+		putchar(x);
+	}
+putchar('\n');
+
+return (0)
+}
