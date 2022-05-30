e83f1fdc3af2f114685a90976b16811b4e6e8a33
actatum88 <4946@holbertonschool.com>
2022-05-30 14:17:42 -0700

    variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print combos
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
