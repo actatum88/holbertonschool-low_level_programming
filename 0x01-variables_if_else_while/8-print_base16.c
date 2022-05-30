a292e6cb16b3e36f3f7d0bbda5303cbbb4dded1a
actatum88 <4946@holbertonschool.com>
2022-05-30 14:02:52 -0700

    variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
