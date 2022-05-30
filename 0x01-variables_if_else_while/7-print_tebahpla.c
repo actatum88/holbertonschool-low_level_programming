f044f9e4f0f86c3f62e1ab8f23cf34ca55b497ea
actatum88 <4946@holbertonschool.com>
2022-05-30 13:55:36 -0700

    variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - smile in the mirror
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+return (0);
+}
