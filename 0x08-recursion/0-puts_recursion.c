0679e40d443968b4da0285d35494988902590f97
actatum88 <4946@holbertonschool.com>
2022-06-09 21:11:44 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,17 @@
  *
  *Return: Always 0.
  */
+
 void _puts_recursion(char *s)
 {
-
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-	       	_putchar ('\n');
-		return;
-	}
 		_putchar(*s);
-		_puts_recursion(s++);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
