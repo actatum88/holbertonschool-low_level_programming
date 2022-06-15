730bc6acf87163fea3ec1a72f03c96fb54d889c8
actatum88 <4946@holbertonschool.com>
2022-06-15 10:24:24 -0700

    argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print its name
+ *@argc: arguments
+ *@argv: array pointing to arguments
+ *
+ *Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i, res = 1;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+	return (0);
+}
