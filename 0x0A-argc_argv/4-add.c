e80809ee03532a7d7ca3e9c4840ab790ce8ba823
actatum88 <4946@holbertonschool.com>
2022-06-15 12:27:16 -0700

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,12 +12,12 @@ int main(int argc, char **argv)
 {
 	int i, sum = 0;
 
-	if (argv < 1)
+	if (argc < 1)
 		return (0);
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		if (atoi(argv[i]))
 		{
 			printf("%s\n", "Error");
 			return (1);
