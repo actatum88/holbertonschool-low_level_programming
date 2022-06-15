bc1ded8df244d234363c95c787767f18b8b4100f
actatum88 <4946@holbertonschool.com>
2022-06-15 08:22:38 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print its name
+ *@argc: print
+ *@argv: print
+ *
+ *Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
