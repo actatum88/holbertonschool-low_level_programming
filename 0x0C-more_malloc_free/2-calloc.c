e3672acac1dc79d8d7ea2e46bc983afdd9658e55
actatum88 <4946@holbertonschool.com>
2022-06-27 18:03:35 -0700

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,26 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include "main.h"
 /**
-* more_malloc_free - allocate memory for an array, using malloc
-* @size: size of array
-* @c: character
-*
-* Return: NULL if array write fails or size is 0, else array
-*/
+ * _calloc - print 0s into 2D grid
+ * @nmemb: array num
+ * @size: size of ea array element
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
+
+	return (arr);
+}
