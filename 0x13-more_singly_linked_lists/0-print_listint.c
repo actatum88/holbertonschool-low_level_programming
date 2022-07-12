9bf55aceb4e6a7c5e8b23e12ad12313028f61c40
actatum88 <4946@holbertonschool.com>
2022-07-12 10:27:39 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,15 @@
  */
 
 size_t print_listint(const listint_t *h)
-
 {
-size_t num = 0;
+	size_t number = 0;
+	const listint_t *temp = h;
 
-while (h != NULL)
-{
-	printf("%d\n", h->n);
-num++;
-h = h->next;
-}
-
-return (num);
+	while (temp != NULL)
+	{
+		printf("%u\n", h->n);
+		temp = temp->next;
+		number++;
+	}
+	return (number);
 }
