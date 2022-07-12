3d5a4ab5796bec2d60ddeae22bc5e0e3e92d0c6c
actatum88 <4946@holbertonschool.com>
2022-07-12 11:46:07 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,8 +8,7 @@
  * Return: pointer to the new node, or NULL if it fails
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
-
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *tail = *head;
