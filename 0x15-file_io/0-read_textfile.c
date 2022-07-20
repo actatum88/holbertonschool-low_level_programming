fd999e207d4b79da1222b096123429ce69c0247d
actatum88 <4946@holbertonschool.com>
2022-07-19 20:54:24 -0700

    io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,8 +13,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	file = open(filename, O_RDONLY);
+	file = popen(filename, O_RDONLY);
 
 	if (file == -1)
 		return (0);
-	
+
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+		return (0);
+
+	rd = fread(file, buf, letters);
+	if (rd == -1)
+		return (0);
+
+	buf[letters] = '\0';
+
+	wr = fwrite(1, buf, rd);
+	if (wr == -1)
+		return (0);
+
+	pclose(file);
+	free(buf);
+	return (wr);
+}
