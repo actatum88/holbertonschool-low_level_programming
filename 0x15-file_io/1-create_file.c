35df0666a8b176aa5ab8d23634876644f015fd1e
actatum88 <4946@holbertonschool.com>
2022-08-09 10:59:37 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,11 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	int write_it;
-	int text_length = 0;
+	int fd, fw, l;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content == NULL)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+		return (-1);
+	if (!text_content)
 	{
+		close(fd);
+		return (1);
+	}
+	for (l = 0; text_content[l] != '\0'; l++)
+	;
+	fw = write(fd, text_content, l);
+	close(fd);
+	if (fw < 0)
+		return (-1);
+	return (1);
+}
